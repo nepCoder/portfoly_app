dbc968cae9b5ae877f5f55fa75f1d76a1e0597fa
Bikash Khanal <iambkhanal@gmail.com>
2024-03-29 11:17:39 +0545

    added .env

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div style={{ backgroundColor: 'green' }}>
+          <span style={{color:'white'}}>{process.env.DB_NAME}</span>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
